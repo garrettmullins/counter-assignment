c1857c57f4246b0d4af40de259536d9062ec4de4
garrettmullins <gmullins@ufl.edu>
2024-11-20 23:32:10 -0500

    added counter tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,35 @@
 // import necessary react testing library helpers here
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 // import the Counter component here
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/Counter/i)).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByTestId('count')).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const addButton = screen.getByRole('button', { name: /\+/ });
+  userEvent.click(addButton);
+  expect(screen.getByTestId('count')).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const subButton = screen.getByRole('button', { name: /-/ });
+  userEvent.click(subButton);
+  expect(screen.getByTestId('count')).toHaveTextContent('-1');
 });
